c7163c11e58318bf5e08c1a4f3b462128c5fa1b5
colornos <rivas432@hotmail.com>
2023-05-12 13:58:44 -0500

    Update BS430.py

--- BS430.py
+++ BS430.py
@@ -1,71 +1,95 @@
-import logging
+import multiprocessing
 import sys
-import time
+import pygatt.backends
+import logging
 from configparser import ConfigParser
-from struct import pack
+import time
+import subprocess
+from struct import *
+from binascii import hexlify
 import os
-import importlib
-import pygatt
+import threading
+from time import sleep
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
 
-# Relevant characteristics submitted by the scale
-# (Explanation see below)
-Char_person = '00008a82-0000-1000-8000-00805f9b34fb'  # person data
+GPIO.setwarnings(False)
+
+# Interesting characteristics
 Char_weight = '00008a21-0000-1000-8000-00805f9b34fb'  # weight data
-Char_body = '00008a22-0000-1000-8000-00805f9b34fb'  # body data
 Char_command = '00008a81-0000-1000-8000-00805f9b34fb'  # command register
 
+def sanitize_timestamp(timestamp):
+    retTS = time.time()
+    return retTS
+
 def decodeWeight(handle, values):
-    data = unpack('<BHxxIxxxxB', bytes(values[0:14]))
+    data = unpack('<BHHxxxxxIH', bytes(values[0:16]))
     retDict = {}
-    retDict["valid"] = (data[0] == 0x1d)
-    # Weight is reported in 10g. Hence, divide by 100.0
-    # To force results to be floats: devide by float.
-    retDict["weight"] = data[1]/100.0
+    retDict["valid"] = (data[0] == 0x1e)
+    retDict["weight"] = data[1]
     retDict["timestamp"] = sanitize_timestamp(data[2])
     retDict["person"] = data[3]
     return retDict
 
+def processIndication(handle, values):
+    if handle == handle_weight:
+        result = decodeWeight(handle, values)
+        if result not in weightdata:
+            log.info(str(result))
+            weightdata.append(result)
+        else:
+            log.info('Duplicate weightdata record')
+    else:
+        log.debug('Unhandled Indication encountered')
 
-# Load the functions from the original script
-def init_ble_mode():
-    global ble_mode
-    ble_mode = 'Indication'
-
-    log.info('BLE mode set to: %s' % ble_mode)
-    return True
-
-
-def wait_for_device(device_name):
-    log.info('Waiting for device %s to become available...' % device_name)
-    while True:
-        devices = adapter.scan(run_as_root=True, timeout=3)
-        for dev in devices:
-            if dev['name'] == device_name:
-                log.info('Found %s' % device_name)
-                return
-        time.sleep(3)
-
-def connect_device(ble_address):
-    global device, handle_command  # Add handle_command to the list of global variables
-    device = None
-    while device is None:
+def wait_for_device(devname):
+    found = False
+    while not found:
         try:
-            device = adapter.connect(ble_address, address_type=addresstype)
-            log.info('Connected to device %s' % ble_address)
+            # wait for bpm to wake up and connect to it
+            found = adapter.filtered_scan(devname)
+        except pygatt.exceptions.BLEError:
+            # reset adapter when (see issue #33)
+            adapter.reset()
+    return
+
+def connect_device(address):
+    device_connected = False
+    tries = 3
+    device = None
+    while not device_connected and tries > 0:
+        try:
+            device = adapter.connect(address, 8, addresstype)
+            device_connected = True
         except pygatt.exceptions.NotConnectedError:
-            log.warning('Failed to connect, retrying...')
-            time.sleep(1)
-    handle_command = device.get_handle(Char_command)
+            tries -= 1
     return device
 
-#handle_command = device.get_handle(Char_command)
-# Read .ini file and set plugins-folder
+def init_ble_mode():
+    p = subprocess.Popen("sudo btmgmt le on", stdout=subprocess.PIPE,
+                         shell=True)
+    (output, err) = p.communicate()
+    if not err:
+        log.info(output)
+        return True
+    else:
+        log.info(err)
+        return False
+
+'''
+Main program loop
+'''
+
 config = ConfigParser()
 config.read('BS430.ini')
 path = "plugins/"
+plugins = {}
 
-# Set up logging
-numeric_level = getattr(logging, config.get('Program', 'loglevel').upper(), None)
+# set up logging
+numeric_level = getattr(logging,
+                        config.get('Program', 'loglevel').upper(),
+                        None)
 if not isinstance(numeric_level, int):
     raise ValueError('Invalid log level: %s' % loglevel)
 logging.basicConfig(level=numeric_level,
@@ -80,65 +104,52 @@ formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(funcName)s %(messag
 ch.setFormatter(formatter)
 log.addHandler(ch)
 
-# Load scale information from .ini-file
+# Load configured plugins
+
+if config.has_option('Program', 'plugins'):
+    config_plugins = config.get('Program', 'plugins').split(',')
+    config_plugins = [plugin.strip(' ') for plugin in config_plugins]
+    log.info('Configured plugins: %s' % ', '.join(config_plugins))
+
+    sys.path.insert(0, path)
+    for plugin in config_plugins:
+        log.info('Loading plugin: %s' % plugin)
+        mod = __import__(plugin)
+        plugins[plugin] = mod.Plugin()
+    log.info('All plugins loaded.')
+else:
+    log.info('No plugins configured.')
+sys.path.pop(0)
+
 ble_address = config.get('Scale', 'ble_address')
 device_name = config.get('Scale', 'device_name')
 device_model = config.get('Scale', 'device_model')
 
-if device_model == 'BS410':
+if device_model == '444':
     addresstype = pygatt.BLEAddressType.public
-    time_offset = 1262304000
-elif device_model == 'BS444':
-    addresstype = pygatt.BLEAddressType.public
-    time_offset = 1262304000
+    # On BS410 time=0 equals 1/1/2010. 
+    # time_offset is used to convert to unix standard
+    time_offset = 0
 else:
     addresstype = pygatt.BLEAddressType.random
     time_offset = 0
-
-# Global variable to store the last weight timestamp
-last_weight_timestamp = 0
-
-plugins = []
-
-for plugin_name in os.listdir(path):
-    if plugin_name.endswith(".py"):
-        module_name = plugin_name[:-3]
-        module = importlib.import_module(f"{path.replace('/', '.')}{module_name}")
-        plugins.append(module)
-
-def processIndication(handle, data):
-    global weightdata, last_weight_timestamp
-
-    if handle == handle_weight:
-        weight = decodeWeight(handle, data)  # Change this line
-        if weight['timestamp'] > last_weight_timestamp:
-            last_weight_timestamp = weight['timestamp']
-            weightdata = [weight]  # Replace weightdata with the new weight
-
-log.info('BS440 Started')
+'''
+Start BLE comms and run that forever
+'''
+log.info('BS430 Started')
 if not init_ble_mode():
     sys.exit()
 
 adapter = pygatt.backends.GATTToolBackend()
 adapter.start()
 
-device = None
-
 while True:
     wait_for_device(device_name)
     device = connect_device(ble_address)
     if device:
-        handle_command = device.get_handle(Char_command)
         weightdata = []
-        try:  # Add the try block
-            handle_weight = device.get_handle(Char_weight)
-            continue_comms = True
-        except pygatt.exceptions.NotConnectedError:
-            log.warning('Error getting handles')
-            continue_comms = False
-
-        log.info('Continue Comms: ' + str(continue_comms))
-        if (not continue_comms): continue
+        handle_weight = device.get_handle(Char_weight)
+        continue_comms = True
 
         try:
             device.subscribe(Char_weight,
@@ -148,30 +159,18 @@ while True:
             continue_comms = False
 
         if continue_comms:
-            timestamp = bytearray(pack('<I', int(time.time() - time_offset)))
-            timestamp.insert(0, 2)
+            log.info('Waiting for notifications for another 30 seconds')
+            time.sleep(30)
             try:
-                device.char_write_handle(handle_command, timestamp,
-                                         wait_for_response=True)
-            except pygatt.exceptions.NotificationTimeout:
-                pass
+                device.disconnect()
             except pygatt.exceptions.NotConnectedError:
-                continue_comms = False
-            if continue_comms:
-                log.info('Waiting for notifications for another 30 seconds')
-                time.sleep(30)
-                try:
-                    device.disconnect()
-                except pygatt.exceptions.NotConnectedError:
-                    log.info('Could not disconnect...')
+                log.info('Could not disconnect...')
 
-                log.info('Done receiving data from scale')
+            log.info('Done receiving data from the scale')
+            if weightdata:
+                weightdatasorted = sorted(weightdata, key=lambda k: k['timestamp'], reverse=True)
 
-                # Process the most recent weight data
-                if weightdata:
-                    log.info(f"Most recent weight data: {weightdata[0]}")
-                    for plugin in plugins:
-                        plugin.process_data(weightdata[0])
-                else:
-                    log.error('Unreliable data received. Unable to process')
-adapter.stop()
+                for plugin in plugins.values():
+                    plugin.execute(config, weightdatasorted)
+            else:
+                log.error('Unreliable data received. Unable to process')
