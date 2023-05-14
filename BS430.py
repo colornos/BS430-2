e3f2e54dc18d02e3f0adf839010bade5820bc70e
colornos <rivas432@hotmail.com>
2023-05-13 19:35:05 -0500

    Update BS430.py

--- BS430.py
+++ BS430.py
@@ -13,29 +13,60 @@ from time import sleep
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
-GPIO.setwarnings(False)
-
-# Interesting characteristics
+# Relevant characteristics submitted by the scale
+Char_person = '00008a82-0000-1000-8000-00805f9b34fb'  # person data
 Char_weight = '00008a21-0000-1000-8000-00805f9b34fb'  # weight data
 Char_command = '00008a81-0000-1000-8000-00805f9b34fb'  # command register
 
-def sanitize_timestamp(timestamp):
-    retTS = time.time()
-    return retTS
+def decodePerson(handle, values):
+
+    data = unpack('BxBxBBBxB', bytes(values[0:9]))
+    retDict = {}
+    retDict["valid"] = (data[0] == 0x84)
+    retDict["person"] = data[1]
+    if data[2] == 1:
+        retDict["gender"] = "male"
+    else:
+        retDict["gender"] = "female"
+    retDict["age"] = data[3]
+    retDict["size"] = data[4]
+    if data[5] == 3:
+        retDict["activity"] = "high"
+    else:
+        retDict["activity"] = "normal"
+    return retDict
 
 def decodeWeight(handle, values):
+
     data = unpack('<BHxxIxxxxB', bytes(values[0:14]))
     retDict = {}
     retDict["valid"] = (data[0] == 0x1d)
-    # Weight is reported in 10g. Hence, divide by 100.0
-    # To force results to be floats: devide by float.
-    retDict["weight"] = data[1] / 100.0
+    retDict["weight"] = data[1]/100.0
     retDict["timestamp"] = sanitize_timestamp(data[2])
     retDict["person"] = data[3]
     return retDict
 
+def sanitize_timestamp(timestamp):
+
+    retTS = 0
+    if timestamp + time_offset < sys.maxsize:
+        retTS = timestamp + time_offset
+    else:
+        retTS = timestamp
+    if timestamp >= sys.maxsize:
+        retTS = 0
+    return retTS
+
 def processIndication(handle, values):
-    if handle == handle_weight:
+
+    if handle == handle_person:
+        result = decodePerson(handle, values)
+        if result not in persondata:
+            log.info(str(result))
+            persondata.append(result)
+        else:
+            log.info('Duplicate persondata record')
+    elif handle == handle_weight:
         result = decodeWeight(handle, values)
         if result not in weightdata:
             log.info(str(result))
@@ -49,10 +80,8 @@ def wait_for_device(devname):
     found = False
     while not found:
         try:
-            # wait for bpm to wake up and connect to it
             found = adapter.filtered_scan(devname)
         except pygatt.exceptions.BLEError:
-            # reset adapter when (see issue #33)
             adapter.reset()
     return
 
@@ -78,67 +107,10 @@ def init_ble_mode():
     else:
         log.info(err)
         return False
-
-'''
-Main program loop
-'''
-
-config = ConfigParser()
-config.read('BS430.ini')
-path = "plugins/"
-plugins = {}
-
-# set up logging
-numeric_level = getattr(logging,
-                        config.get('Program', 'loglevel').upper(),
-                        None)
-if not isinstance(numeric_level, int):
-    raise ValueError('Invalid log level: %s' % loglevel)
-logging.basicConfig(level=numeric_level,
-                    format='%(asctime)s %(levelname)-8s %(funcName)s %(message)s',
-                    datefmt='%a, %d %b %Y %H:%M:%S',
-                    filename=config.get('Program', 'logfile'),
-                    filemode='w')
-log = logging.getLogger(__name__)
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(numeric_level)
-formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(funcName)s %(message)s')
-ch.setFormatter(formatter)
-log.addHandler(ch)
-
-# Load configured plugins
-
-if config.has_option('Program', 'plugins'):
-    config_plugins = config.get('Program', 'plugins').split(',')
-    config_plugins = [plugin.strip(' ') for plugin in config_plugins]
-    log.info('Configured plugins: %s' % ', '.join(config_plugins))
-
-    sys.path.insert(0, path)
-    for plugin in config_plugins:
-        log.info('Loading plugin: %s' % plugin)
-        mod = __import__(plugin)
-        plugins[plugin] = mod.Plugin()
-    log.info('All plugins loaded.')
-else:
-    log.info('No plugins configured.')
-sys.path.pop(0)
-
-ble_address = config.get('Scale', 'ble_address')
-device_name = config.get('Scale', 'device_name')
-device_model = config.get('Scale', 'device_model')
-
-if device_model == '444':
-    addresstype = pygatt.BLEAddressType.public
-    # On BS410 time=0 equals 1/1/2010. 
-    # time_offset is used to convert to unix standard
-    time_offset = 0
-else:
-    addresstype = pygatt.BLEAddressType.random
-    time_offset = 0
 '''
 Start BLE comms and run that forever
 '''
-log.info('BS430 Started')
+log.info('BS440 Started')
 if not init_ble_mode():
     sys.exit()
 
@@ -148,31 +120,71 @@ adapter.start()
 while True:
     wait_for_device(device_name)
     device = connect_device(ble_address)
+    # If the device was connected successfully (the variable "device" has
+    # been defined an contains the instance of the BLEDevice) the main loop runs
     if device:
+        persondata = []
         weightdata = []
-        handle_weight = device.get_handle(Char_weight)
-        continue_comms = True
+        try:
+            # Get the two-byte shortcut (the handle)
+            handle_person = device.get_handle(Char_person)
+            handle_weight = device.get_handle(Char_weight)
+            handle_command = device.get_handle(Char_command)
+            continue_comms = True
+        except pygatt.exceptions.NotConnectedError:
+            log.warning('Error getting handles')
+            continue_comms = False
 
+        log.info('Continue Comms: ' + str(continue_comms))
+        if (not continue_comms): continue
+
+        '''
+        subscribe to characteristics and have processIndication
+        process the data received.
+        '''
         try:
             device.subscribe(Char_weight,
+                             callback=processIndication,
+                             indication=True)
+            device.subscribe(Char_person,
                              callback=processIndication,
                              indication=True)
         except pygatt.exceptions.NotConnectedError:
             continue_comms = False
 
+        '''
+        Send the unix timestamp in little endian order preceded by 02 as
+        bytearray to handle 0x23. This will resync the scale's RTC.
+        While waiting for a response notification, which will never
+        arrive, the scale will emit 30 Indications on 0x1b (weight)
+        and 0x1e (body) each.
+        '''
         if continue_comms:
-            log.info('Waiting for notifications for another 30 seconds')
-            time.sleep(30)
+            timestamp = bytearray(pack('<I', int(time.time() - time_offset)))
+            timestamp.insert(0, 2)
             try:
-                device.disconnect()
+                device.char_write_handle(handle_command, timestamp,
+                                         wait_for_response=True)
+            except pygatt.exceptions.NotificationTimeout:
+                pass
             except pygatt.exceptions.NotConnectedError:
-                log.info('Could not disconnect...')
+                continue_comms = False
+            if continue_comms:
+                log.info('Waiting for notifications for another 30 seconds')
+                time.sleep(30)
+                try:
+                    device.disconnect()
+                except pygatt.exceptions.NotConnectedError:
+                    log.info('Could not disconnect...')
 
-            log.info('Done receiving data from the scale')
-            if weightdata:
-                weightdatasorted = sorted(weightdata, key=lambda k: k['timestamp'], reverse=True)
-
-                for plugin in plugins.values():
-                    plugin.execute(config, weightdatasorted)
-            else:
-                log.error('Unreliable data received. Unable to process')
+                log.info('Done receiving data from scale')
+                # process data if all received well
+                if persondata and weightdata:
+                    # Sort scale output by timestamp to retrieve most recent three results
+                    weightdatasorted = sorted(weightdata, key=lambda k: k['timestamp'], reverse=True)
+                    
+                    # Run all plugins found
+                    for plugin in plugins.values():
+                        plugin.execute(config, persondata, weightdatasorted)
+                else:
+                    log.error('Unreliable data received. Unable to process')
